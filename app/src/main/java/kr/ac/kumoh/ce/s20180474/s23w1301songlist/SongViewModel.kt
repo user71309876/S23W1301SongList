433e2d2ee46355e8ccdf080d9498377a92a3b7e5
user71309876 <93814905+user71309876@users.noreply.github.com>
2023-11-29 15:18:21 +0900

    SongList Complete

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180474/s23w1301songlist/SongViewModel.kt
@@ -0,0 +1,39 @@
+package kr.ac.kumoh.ce.s20180474.s23w1301songlist
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+class SongViewModel() :ViewModel() {
+    private val SERVER_URL="https://port-0-swp-1drvf2llomiqzf1.sel5.cloudtype.app/"
+    private val songApi: SongApi
+    private val _songList=MutableLiveData<List<Song>>()
+    val songList: LiveData<List<Song>> get()=_songList
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        songApi = retrofit.create(SongApi::class.java)
+        fetchData()
+    }
+
+    private fun fetchData() {
+        viewModelScope.launch{
+            try{
+                val response=songApi.getSongs()
+                _songList.value=response
+            }catch(e: Exception){
+                Log.e("fetchData()",e.toString())
+            }
+        }
+    }
+}

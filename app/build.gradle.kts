e99559acdea38858ff58c8c9111fe709a5b88362
user71309876 <93814905+user71309876@users.noreply.github.com>
2023-11-29 16:47:57 +0900

    finish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
